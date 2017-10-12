d0803b7839d582fd7480b7c3988bb45e4316b69a
michael-mungai <michaelmungai@Michaels-MBP.wireless.newschool.edu>
2017-10-12 17:49:33 -0400

    Assignment

--- grid-exercise/grid-exercise.js
+++ grid-exercise/grid-exercise.js
@@ -104,7 +104,47 @@ for (var i = 0; i < 10; i = i + 1) {
         console.log("Row " + i + ", Column " + j);
 
         // The next line is the only JS you'll have to change.
-        if (i % i < i) {
+        if (i % 2 === 0 && j < 5 || i % 2 != 0 && j >= 5) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+document.write('<h2>6</h2>');
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i % 2 === 0 && j % 2 || i % 2 != 0 && j % 2 != 0) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+document.write('<h2>6</h2>');
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i % 2 === 0 && j % 2 === 0 || i % 2 != 0 && j % 2 != 0) {
             console.log("   pink");
             document.write("<div class='box pink'></div>");
         } else {
--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,22 @@
-var animals = ["cat","dog ","pig","toucan", "pecan", "crocodile"];
-console.log(animals);
 
-for (var i = 0; i < animals.length; i++) {
-  var animal = animals[i];
-  console.log("<img src='" + animal + ".jpg' />")
+function updateTime () {
+  var clockHolder = document.getElementById("clock");
+  var now = new Date ();
+  var seconds = now.getSeconds();
+  now = now.toTimeString();
+
+  if (seconds < 20) {
+    //red
+    clockHolder.style.color = "red";
+  } else if (seconds < 40) {
+    //green
+    clockHolder.style.color = "green";
+  } else {
+    //blue
+    clockHolder.style.color = "blue";
+  }
+
+  clockHolder.innerHTML = now;
 }
+
+setInterval(updateTime, 1000);
