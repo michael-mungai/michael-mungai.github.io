bc5e45ff757ba38f5312d59d0580de51286d0a17
michael-mungai <31457846+michael-mungai@users.noreply.github.com>
2017-12-19 21:11:50 -0500

    okokok
    okokkoko

--- toy/toy.js
+++ toy/toy.js
@@ -1,45 +1,45 @@
-//variables
-userSeq = [];
-gameSeq = [0,1,2];
-var id = 0;
-var color = 0;
+var userSeq = []; //stores user sequence needed to compare the game startSequence
+var gameSeq = []; //sequence of the game for user to duplicate
+var lvlNum = 10;
+var id;
+var color;
 var level = 0;
-var startButton = document.getElementById("start");
-var levelDisplay = document.getElementById("level-number");
+var start = document.getElementById("start");
+var levelCounter = document.getElementById("level-number");
 var box = document.getElementsByClassName("box");
 
-startButton.addEventListener("click", startGame);
+start.addEventListener("click", startGame);
 function startGame() {
-  level++;
+  level++; //increases level by one each time start is pressed;
   startSequence();
 }
 
-for (var j = 0; j < box.length; j++) {
-  box[j].addEventListener("click", userBox);
+for (var i = 0; i <box.length; i++) {
+  box[i].addEventListener("click", userSequence);
 }
-
-function userBox() {
-  id = this.getAttribute("id");
-  color = this.getAttribute("class").split(" ")[0];
-  userSeq.push(id);
-  console.log(id + " " + color);
+function userSequence() {
+  id = this.getAttribute("id"); //upon clicking, set var id to the id of whatever box you are clicking
+  color = this.getAttribute("class").split(" ")[0]; //upon clicking, set var color to the class color name of the box you are clicking
+  var boxColor = this.getAttribute("class").split(" ")[1]; //tell the console what box color you are clicking
+  userSeq.push(id); //each time you click on a box, add that box id number to the array userSeq
   addClass(id, color);
-  //check user startSequence
-  if (!checkUserSeq()) {
+  if(!checkUserSeq()) { //check user sequence
     displayError();
-    useSeq = [];
+    userSeq = [];
   }
-  //check end of sequence
-  if (userSeq.length == gameSeq.length) {
+  if((userSeq.length == gameSeq.length) && (userSeq.length < lvlNum)) { //check end of sequence
     level++;
     userSeq = [];
     startSequence();
   }
+  if (userSeq.length == lvlNum) {
+    levelCounter.textContent = "WIN!"
+  }
+  console.log(id + " " + color + " " + boxColor + " " + userSeq);
 }
 
-//Check user seq against simons
 function checkUserSeq() {
-  for (var i = 0; i < userSeq.lengthl; i++) {
+  for (var i = 0; i < userSeq.length; i++) {
     if (userSeq[i] != gameSeq[i]) {
       return false;
     }
@@ -47,15 +47,14 @@ function checkUserSeq() {
   return true;
 }
 
-//Display Error
-function displayError() {
+function displayError() { //display error if wrong sequence is entered
   console.log("error");
   var counter = 0;
   var myError = setInterval(function(){
-    levelDisplay.textContent = "error";
+    levelCounter.textContent = "error";
     counter++;
     if(counter == 3) {
-      levelDisplay.textContent = level;
+      levelCounter.textContent = level;
       clearInterval(myError);
       userSeq = [];
       counter = 0;
@@ -63,37 +62,32 @@ function displayError() {
   }, 500);
 }
 
-
 function startSequence() {
-  levelDisplay.textContent = level;
-  getRandomNum();
-  var i = 0;
-  var myInterval = setInterval(function(){
-    id = gameSeq[i];
-    color = document.getElementById(id).getAttribute("class");
-    color = color.split(" ")[0];
+  console.log(level);
+  levelCounter.textContent = level; //displays level
+  getRandNum();
+  var i = 0; //set a blank variable equal to 0;
+  var int = setInterval(function(){ //loop for displaying random colors in a sequence
+    id = gameSeq[i]; //select a box in the sequence
+    color = document.getElementById(id).getAttribute("class").split(" ")[1]; //set var color to the box selected above, return the second class in the HTML information
     console.log(id + " " + color);
     addClass(id, color);
-    i++;
-    if (i = gameSeq.length) {
-      clearInterval(myInterval);
+    i++; //increment i by 1 each second
+    if(i === gameSeq.length) {
+      clearInterval(int);
     }
-  }, 1000);
+  }, 1000); //each second
 
 }
 
-//generate random number
-function getRandomNum() {
-  var random = Math.floor(Math.random() * 4);
-  gameSeq.push(random);
+function getRandNum() {
+  var random = Math.floor(Math.random() * 4); //generate a random number between 0 and 1 NOT INCLUDING 1, multiply by 4 to get a random number between 0 and 4 NOT INCLUDING 4
+  gameSeq.push(random); //Push random number to gameSeq array created at the top
 }
 
-//Add temporary addClass
-function addClass(id, color) {
-  document.getElementById(id).classList.add(color + "-active");
-
-  setTimeout(function(){
-    document.getElementById(id).classList.remove(color + "-active");
-  });
+function addClass(id, color) { //add a temprorary class (make the box flash)
+  document.getElementById(id).classList.add(color+ "-active"); //get the class of the id depending on the randomly generated number and apply the bright-color class to it, making it flash
+  setTimeout(function() {
+    document.getElementById(id).classList.remove(color+ "-active");
+  }, 500);
 }
-//2 - use replicates sequence
